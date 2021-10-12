514ea673f068907b7c2d6a7ba249246c1b1566ca
onlycan17 <onlycan17@gmail.com>
2021-10-12 15:43:29 +0900

    로그인 페이지

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./screens/NotFound";
 import { ThemeProvider } from "styled-components";
 import {darkTheme, GlobalStyles, lightTheme} from "./styles";
 import SignUp from "./screens/SignUp";
+import routes from "./routes";
 
 
 
@@ -18,11 +19,11 @@ function App() {
       <GlobalStyles />
       <Router>
         <Switch>
-          <Route path="/" exact>
+          <Route path={routes.home} exact>
             {isLoggedIn ? <Home /> : <Login />}
           </Route>
           { !isLoggedIn ? ( 
-              <Route path="/sign-up">
+              <Route path={routes.signUp}>
                 <SignUp />
               </Route>  
           ) : null }
