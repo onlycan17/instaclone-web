8c8549ba6062b5ce2a7c3e53128cdc9591a99e57
onlycan17 <onlycan17@gmail.com>
2021-10-19 21:36:39 +0900

    hashtags link

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -2,25 +2,45 @@ import { checkPropTypes } from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
 import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
-function Comment({author, payload}) {
-    return(
-        <CommentContainer>
-            <FatText>{author}</FatText>
-            <CommentCaption>{payload}</CommentCaption>
-        </CommentContainer>
-    )
+function Comment({ author, payload }) {
+  return (
+    <CommentContainer>
+      <FatText>{author}</FatText>
+      <CommentCaption>
+        {payload.split(" ").map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
+    </CommentContainer>
+  );
 }
 
 Comment.prototypes = {
-    author: PropTypes.string.isRequire,
-    payload: PropTypes.string.isRequired,
-}
+  author: PropTypes.string.isRequire,
+  payload: PropTypes.string.isRequired,
+};
 
 export default Comment;
